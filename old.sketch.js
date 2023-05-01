2260e48b677a1ada3b81d0ccf27b5cdfb104b160
Blite33 <w.harris@hotmail.ca>
2023-05-01 15:20:18 -0600

    Switching mode of display along with mapping.
    Physics will not be considered, only movement left to right will be.

--- old.sketch.js
+++ old.sketch.js
@@ -16,22 +16,12 @@ console.log('instantiating globals...')
 let grid = [];
 let pixelSquare = 50
 
-//Find whichever dimension the screen is smaller in?
-let maximumScreenSize;
-
 console.log('constructing classes...')
 class Pixel {
   constructor(x, y){
     this.color = 'black'
     this.x = x
     this.y = y
-  }
-
-  display() {
-    stroke(this.color);
-    strokeWeight(1)
-    fill(this.color);
-    rect(this.x, this.y, (maximumScreenSize/pixelSquare));
   }
 }
 
@@ -40,38 +30,42 @@ let circleSprite;
 function setup() {
   console.log('setting up...')
   createCanvas(windowWidth, windowHeight);
-  noStroke();
-  if (width < height){
-    maximumScreenSize = width;
-  }
-  if (width > height){
-    maximumScreenSize = height;
-  }
+
+  circleSprite = new Sprite();
+  circleSprite.diameter = 40;
+
+  world.gravity.y = 10
+
+  screenFloor = new Sprite();
+  screenFloor.y = height
+  screenFloor.w = width
+  screenFloor.collider = 'static'
 
   // We should have a function that fills the entire grid with black pixels. Make a cooler name than 'fillWithBlack()'
   // Note from later: function called 'fillGridBlackPixels()'
-  fillGridBlackPixels()
 }
 
 function draw() {
   // Background might be nice. IDK if that will change much if everything is just grid of pixels.
   // It might be better to have a grid background fill function that's called to change the background.
   // There should also be a fade to black kind of function that will fade all pixels to black.
-  background(255);
-  for(let y=0; y<pixelSquare; y++){
-    for(let x=0; x<pixelSquare; x++){
-      grid[y][x].display()
-    }
+  background(220);
+  if (kb.pressing('left')){
+    circleSprite.vel.x = -5;
   }
-  
+  else if (kb.pressing('right')){
+    circleSprite.vel.x = 5;
+  }
+  else if (kb.pressing('up')){
+    circleSprite.vel.y += 9;
+  }
 }
 
 function fillGridBlackPixels() {
   for(let y=0; y<pixelSquare; y++){
     grid.push([])
     for(let x=0; x<pixelSquare; x++){
-      grid[y].push(new Pixel(round(x*(maximumScreenSize/pixelSquare), 2), round(y*(maximumScreenSize/pixelSquare), 2)))
+      grid.push(new Pixel(x,y))
     }
   }
-  console.log(grid);
 }
