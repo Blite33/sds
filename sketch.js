818e0167915b39c101ed4a15b9ccd886c4eb8afa
Blite33 <w.harris@hotmail.ca>
2023-05-18 15:19:22 -0600

    Coding
    Added some code, allowing me to get started on moving items left and right.

--- sketch.js
+++ sketch.js
@@ -19,15 +19,24 @@ let grassTexture;
 
 console.log('constructing classes...');
 class Texture {
-  constructor(x, y, theImage){
+  constructor(x, y, theImage, scale){
     this.color = 'black'
     this.x = x
     this.y = y
     this.image = theImage
+    this.scale = scale
+    this.goalX = 0
+    this.goalY = (height*2/3)
   }
 
   display() {
-    image(this.image, this.x, this.y);
+    image(this.image, this.x, this.y, this.scale, this.scale);
+  }
+
+  move() {
+    // NEW make a system to move a single texture around the screen left and right by using mouse position.
+    this.x += 1
+    this.y += 1
   }
 }
 
@@ -43,7 +52,9 @@ function setup() {
   }
 
   console.log('loading assets...');
-  grassTexture = new Texture(0, 0, loadImage("assets/pixil-frame-0.png"));
+  grassTexture = new Texture(0, 0, loadImage("assets/pixil-frame-0.png"), 10);
+  sunTexture = new Texture(0, 100, loadImage("assets/sun_softer.png"), 100);
+  texturesToDisplay.push(sunTexture);
   texturesToDisplay.push(grassTexture);
 }
 
@@ -51,7 +62,7 @@ function draw() {
   // Background might be nice. IDK if that will change much if everything is just grid of pixels.
   // It might be better to have a grid background fill function that's called to change the background.
   // There should also be a fade to black kind of function that will fade all pixels to black.
-  background(255);
+  background(0);
   
   for(let x=0; x<texturesToDisplay.length; x++){
     texturesToDisplay[x].display();
