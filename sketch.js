0cc636e5de6f537ba29852d09de5b55474da80a2
Blite33 <w.harris@hotmail.ca>
2023-05-19 15:23:21 -0600

    Movement functions, and new class

--- sketch.js
+++ sketch.js
@@ -8,6 +8,7 @@
 console.log('instantiating globals...');
 // instantiate grid globally...
 let texturesToDisplay = [];
+let mobsToDisplay = [];
 let pixelSquare = 20;
 
 //Find whichever dimension the screen is smaller in?
@@ -15,7 +16,9 @@ let maximumScreenSize;
 let circleSprite;
 let inputtedColor;
 
+// find some better way than using globals.
 let grassTexture;
+let sunTexture;
 
 console.log('constructing classes...');
 class Texture {
@@ -23,6 +26,19 @@ class Texture {
     this.color = 'black'
     this.x = x
     this.y = y
+    this.image = theImage
+    this.scale = scale
+  }
+
+  display() {
+    image(this.image, this.x, this.y, this.scale, this.scale);
+  }
+}
+
+class Mob {
+  constructor(x, theImage, scale){
+    this.x = x
+    this.y = (height*2/3)
     this.image = theImage
     this.scale = scale
     this.goalX = 0
@@ -34,9 +50,14 @@ class Texture {
   }
 
   move() {
-    // NEW make a system to move a single texture around the screen left and right by using mouse position.
-    this.x += 1
-    this.y += 1
+    if(this.x !== this.goalX){
+      if(this.x > this.goalX){
+        this.x -= (this.goalX / this.x) * 3;
+      }
+      else if(this.x < this.goalX){
+        this.x += (this.goalX / this.x) * 3;
+      }
+    }
   }
 }
 
@@ -44,6 +65,7 @@ function setup() {
   console.log('setting up...')
   createCanvas(windowWidth, windowHeight);
   noStroke();
+  noSmooth();
   if (width < height){
     maximumScreenSize = width;
   }
@@ -56,15 +78,33 @@ function setup() {
   sunTexture = new Texture(0, 100, loadImage("assets/sun_softer.png"), 100);
   texturesToDisplay.push(sunTexture);
   texturesToDisplay.push(grassTexture);
+
+  characterTexture = new Mob(100, loadImage("assets/char-pixil-frame-0.png"), 100)
+  mobsToDisplay.push(characterTexture);
 }
 
 function draw() {
-  // Background might be nice. IDK if that will change much if everything is just grid of pixels.
-  // It might be better to have a grid background fill function that's called to change the background.
-  // There should also be a fade to black kind of function that will fade all pixels to black.
   background(0);
   
-  for(let x=0; x<texturesToDisplay.length; x++){
-    texturesToDisplay[x].display();
+  //move
+  for(let i=0; i<mobsToDisplay.length; i++){
+    mobsToDisplay[i].move();
+  }
+  
+  //draw
+  for(let i=0; i<texturesToDisplay.length; i++){
+    texturesToDisplay[i].display();
+  }
+  for(let i=0; i<mobsToDisplay.length; i++){
+    mobsToDisplay[i].display();
+  }
+  // Maybe add another layer for background squares.
+}
+
+function mousePressed(){
+  console.log('INFO mousePressed() ' + mouseX + ' ' + mouseY);
+
+  for(let i=0; i<mobsToDisplay.length; i++){
+    mobsToDisplay[i].goalX = mouseX;
   }
 }
