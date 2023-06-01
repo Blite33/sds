9ad35390487fd93c244fd148fa4b2eca889c65b2
Blite33 <w.harris@hotmail.ca>
2023-06-01 15:19:42 -0600

    Updated movement
    Levels and x axis now fixed
    Idle animation function started

--- sketch.js
+++ sketch.js
@@ -73,10 +73,35 @@ class Bob {
       else{
         this.x = this.goalX;
       }
-      if(this.x > windowWidth - 10){
-        this.x = this.goalX - width;
+      if(this.x > windowWidth - 20){
+        this.x = 20;
+        this.goalX = this.x;
         this.superXLevel++;
         currentLevelX++;
+        for(let i=0; i<texturesToDisplay.length; i++){
+          if(texturesToDisplay[i].label === 'grassTexture'){
+            texturesToDisplay[i].superXLevel = currentLevelX;
+          }
+          if(texturesToDisplay[i].label === 'sunTexture'){
+            texturesToDisplay[i].x += 50;
+          }
+        }
+        console.log(`INFO Bob.move() ${MSG} lvl${currentLevelX-1} > lvl${currentLevelX}`)
+      }
+      if(this.goalX < 20 && currentLevelX > 0){
+        this.x = windowWidth - 21 - this.scale;
+        this.goalX = this.x;
+        this.superXLevel--;
+        currentLevelX--;
+        for(let i=0; i<texturesToDisplay.length; i++){
+          if(texturesToDisplay[i].label === 'grassTexture'){
+            texturesToDisplay[i].superXLevel = currentLevelX;
+          }
+          if(texturesToDisplay[i].label === 'sunTexture'){
+            texturesToDisplay[i].x -= 50;
+          }
+        }
+        console.log(`INFO Bob.move() ${MSG} lvl${currentLevelX+1} > lvl${currentLevelX}`)
       }
     }
     else{
@@ -94,7 +119,7 @@ class Mob {
     this.imgFrame1 = frame1;
     this.image = frame0;
     this.scale = scale;
-    this.goalX = 0;
+    this.goalX = this.x;
     this.goalY = (height*2/3);
     this.superXLevel = xLevel
   }
@@ -124,7 +149,14 @@ class Mob {
   }
 
   idle() {
-
+    if(frameCount % 120 === 0){
+      if(this.image === this.imgFrame0){
+        this.image = this.imgFrame1;
+      }
+      else if(this.image === this.imgFrame1){
+        this.image = this.imgFrame0;
+      }
+    }
   }
 
   dialogue() {
@@ -153,7 +185,7 @@ function setup() {
   mobsToDisplay.push(new Bob('bob', 100, loadImage("assets/char-pixil-frame-0.png"), loadImage("assets/char-pixil-frame-1.png"), 100, 0));
   console.log(`loaded: ${mobsToDisplay[mobsToDisplay.length-1].label}`)
 
-  mobsToDisplay.push(new Mob('cowboy', 100, loadImage("assets/cowboy-pixil-frame-0.png"), loadImage("assets/cowboy-pixil-frame-1.png"), 100, 1));
+  mobsToDisplay.push(new Mob('cowboy', width*2/3, loadImage("assets/cowboy-pixil-frame-0.png"), loadImage("assets/cowboy-pixil-frame-1.png"), 100, 1));
   console.log(`loaded: ${mobsToDisplay[mobsToDisplay.length-1].label}`)
 }
 
@@ -163,6 +195,13 @@ function draw() {
   //move
   for(let i=0; i<mobsToDisplay.length; i++){
     mobsToDisplay[i].move();
+  }
+
+  //idle
+  for(let i=0; i<mobsToDisplay.length; i++){
+    if(mobsToDisplay[i].label !== 'bob'){
+      mobsToDisplay[i].idle();
+    }
   }
   
   //draw
